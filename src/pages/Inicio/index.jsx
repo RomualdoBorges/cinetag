f137bd5337c5b9cacd6afaf62c86f6c14ac73b7f
geomonitora@gmail.com <geomonitora@gmail.com>
2023-05-04 08:16:38 -0300

    Aula 1

---
+++ src/pages/Inicio/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from "./Inicio.module.css";
+import Cabecalho from "components/Cabecalho";
+import Rodape from "components/Rodape";
+
+export default function Inicio() {
+  return (
+    <>
+      <Cabecalho />
+      <Rodape />
+    </>
+  );
+}
