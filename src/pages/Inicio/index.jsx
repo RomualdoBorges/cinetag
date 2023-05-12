23364c4728a92ee5dfd90474b649da7327b956be
geomonitora@gmail.com <geomonitora@gmail.com>
2023-05-12 11:44:40 -0300

    Aula 5

--- src/pages/Inicio/index.jsx
+++ src/pages/Inicio/index.jsx
@@ -3,9 +3,20 @@ import styles from "./Inicio.module.css";
 import Banner from "components/Banner";
 import Titulo from "components/Titulo";
 import Card from "components/Card";
-import videos from "json/db.json";
 
 export default function Inicio() {
+  const [videos, setVideos] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch(
+      "https://my-json-server.typicode.com/RomualdoBorges/cinetag-api/videos"
+    )
+      .then((resposta) => resposta.json())
+      .then((dados) => {
+        setVideos(dados);
+      });
+  }, []);
+
   return (
     <>
       <Banner imagem="home" />
--- src/pages/Player/index.jsx
+++ src/pages/Player/index.jsx
@@ -3,14 +3,21 @@ import styles from "./Player.module.css";
 import Banner from "components/Banner";
 import Titulo from "components/Titulo";
 import { useParams } from "react-router-dom";
-import videos from "json/db.json";
 import NaoEncontrada from "pages/NaoEncontrada";
 
 export default function Player() {
+  const [video, setVideo] = React.useState();
   const parametros = useParams();
-  const video = videos.find((video) => {
-    return video.id === Number(parametros.id);
-  });
+
+  React.useEffect(() => {
+    fetch(
+      `https://my-json-server.typicode.com/RomualdoBorges/cinetag-api/videos?id=${parametros.id}`
+    )
+      .then((resposta) => resposta.json())
+      .then((dados) => {
+        setVideo(...dados);
+      });
+  }, []);
 
   if (!video) {
     return <NaoEncontrada />;
